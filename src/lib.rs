f36def62213d8db9b98b62a1e252db0ba7c02f38
Graham Kelly <gkgoat6700@gmail.com>
2025-04-26 14:58:50 +0000

    boxy

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ use spin::Mutex;
 extern crate alloc;
 static NANS: spin::Mutex<BTreeMap<u32, Entry>> = Mutex::new(BTreeMap::new());
 enum Entry {
-    F64(f64),
+    F64 { float: f64, refc: usize },
     Ptr(NonNull<()>),
 }
 unsafe impl Send for Entry {}
@@ -26,7 +26,7 @@ impl<T> NanBox<T> {
             while l.contains_key(&i) {
                 i += 1
             }
-            l.insert(i, Entry::F64(a));
+            l.insert(i, Entry::F64 { float: a, refc: 1 });
             let b = a.to_bits();
             unsafe {
                 Self {
@@ -46,10 +46,10 @@ impl<T> NanBox<T> {
             i as u32
         };
         let mut l = NANS.lock();
-        let Entry::F64(f) = l.get(&i)? else {
+        let Entry::F64 { float, .. } = l.get(&i)? else {
             return None;
         };
-        Some(*f)
+        Some(*float)
     }
     pub fn with_val(t: T) -> Self {
         let mut l = NANS.lock();
@@ -109,6 +109,7 @@ impl<T> NanBox<T> {
         let p = match l.remove(&i) {
             Some(Entry::Ptr(p)) => p,
             mut p => {
+                // if let Some(Entry::F64 { float, refc })
                 while let Some(p2) = p.take() {
                     p = l.insert(i, p2);
                 }
@@ -121,20 +122,89 @@ impl<T> NanBox<T> {
         let a = unsafe { Box::from_raw(p.as_ptr() as *mut T) };
         Ok(*a)
     }
+    pub unsafe fn from_raw(a: f64) -> Self {
+        Self { raw: a }
+    }
+    pub unsafe fn raw(&self) -> f64 {
+        unsafe { self.raw }
+    }
+    pub unsafe fn take_raw(&mut self) -> f64 {
+        unsafe { take(&mut self.raw) }
+    }
+}
+impl<T: Clone> Clone for NanBox<T> {
+    fn clone(&self) -> Self {
+        if unsafe { self.raw }.is_nan() {
+            let i = {
+                let i = unsafe { self.raw_u64 } & 0xffffffff;
+                i as u32
+            };
+            let mut l = NANS.lock();
+            match l.get_mut(&i) {
+                None => Self {
+                    raw: unsafe { self.raw },
+                },
+                Some(x) => match x {
+                    Entry::F64 { float, refc } => {
+                        *refc += 1;
+                        Self {
+                            raw: unsafe { self.raw },
+                        }
+                    }
+                    Entry::Ptr(non_null) => {
+                        let p = non_null.as_ptr() as *mut T;
+                        drop(l);
+                        Self::with_val(unsafe { &*p }.clone())
+                    }
+                },
+            }
+        } else {
+            Self {
+                raw: unsafe { self.raw },
+            }
+        }
+    }
 }
 impl<T> Drop for NanBox<T> {
     fn drop(&mut self) {
-        let i = unsafe {
-            if !self.raw.is_nan() {
+        let _ = {
+            let i = unsafe {
+                if !self.raw.is_nan() {
+                    return;
+                };
+                let i = self.raw_u64 & 0xffffffff;
+                i as u32
+            };
+            let mut l = NANS.lock();
+            let Some(e) = l.remove(&i) else {
                 return;
             };
-            let i = self.raw_u64 & 0xffffffff;
-            i as u32
+            match e {
+                Entry::Ptr(p) => unsafe { Box::from_raw(p.as_ptr() as *mut T) },
+                Entry::F64 { float, refc } => {
+                    if refc != 0 {
+                        l.insert(
+                            i,
+                            Entry::F64 {
+                                float,
+                                refc: refc - 1,
+                            },
+                        );
+                    }
+                    return;
+                }
+            }
         };
-        let mut l = NANS.lock();
-        let Some(Entry::Ptr(p)) = l.remove(&i) else {
-            return;
-        };
-        let _ = unsafe { Box::from_raw(p.as_ptr() as *mut T) };
     }
 }
+#[cfg(feature = "dumpster")]
+const _: () = {
+    unsafe impl<T: dumpster::Trace> dumpster::Trace for NanBox<T> {
+        fn accept<V: dumpster::Visitor>(&self, visitor: &mut V) -> Result<(), ()> {
+            match self.as_ref() {
+                None => Ok(()),
+                Some(v) => v.accept(visitor),
+            }
+        }
+    }
+};
